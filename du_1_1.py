477371bb13b845ef941ff9db836d033ae28ec16b
cinkcink7 <71768546+cinkcink7@users.noreply.github.com>
2023-10-14 09:44:10 +0200

    Add files via upload

---
+++ du_1_1.py
@@ -0,0 +1,16 @@
+
+# du_X_Y.py
+print("Hallo World!")
+# Povinné domácí úkoly
+# DÚ 2.1: Obvod trojúhelníku
+# Úkol:
+# Vypočítejte obvod trojúhelníku se stranami o délkách a, b, c. Výsledek uložte do
+# proměnné o.
+# Vzorový vstup:
+# 2 3 4
+# Vzorový výstup:
+# 9.0
+# [ ]: a, b, c = [float(x) for x in input().split()] # vstup
+# o = ...
+# print(o) # výstup
+
