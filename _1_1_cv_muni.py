b74813d142c3370b7d3b2a6ce5e00130b9293c35
cinkcink7 <71768546+cinkcink7@users.noreply.github.com>
2023-10-30 09:54:48 +0100

    Add files via upload

---
+++ _1_1_cv_muni.py
@@ -0,0 +1,24 @@
+
+# du_X_Y.py
+print("Hallo world")
+
+a, b = 5, 2
+a += b
+b = a
+a -= 1
+
+print(a, b)
+
+########################################################################
+
+print(5e-6 >= abs(-5e6))
+
+
+n = 20
+n_lost = 7
+
+n_final = n - n_lost
+print(n_final != 13)
+
+
+
